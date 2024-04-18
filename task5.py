425ac05c0e4c250a76c3b8bcd1aff3083d9649a3
AndriusKarpis <kabutas@gmail.com>
2024-04-18 15:34:32 +0300

    * updated function name * added text input from user * added default text

--- task5.py
+++ task5.py
@@ -2,16 +2,19 @@
 # that takes a text as input and extracts all email addresses from the text.
 from typing import List
 
-sentence = "123456 parduotuve pilis masinu telefonas aaaa b21231b bahjs@codeacademy.lt abkjbv@gmail.com"
+sentence = input("Enter text from where to extract Email addresses")
+
+if not sentence:
+    sentence = "123456 parduotuve pilis masinu telefonas aaaa b21231b bahjs@codeacademy.lt abkjbv@gmail.com"
 
 
-def email_extractor(a: str) -> List:
+def extract_email_addresses(a: str) -> List:
     email_list = []
-    splitted_list = a.split(" ")
-    for word in splitted_list:
+    splited_list = a.split(" ")
+    for word in splited_list:
         if "@" in word and "." in word:
             email_list.append(word)
     return email_list
 
 
-print(email_extractor(sentence))
+print(extract_email_addresses(sentence))
